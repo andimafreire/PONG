0cc17c81fd997588117de03b2e449c1536408181
DESKTOP-PBTTV6J\andima <andima.freire@gmail.com>
2018-04-20 11:50:44 +0200

    arreglo rebote general

--- src/packJuego/Pelota.java
+++ src/packJuego/Pelota.java
@@ -4,6 +4,8 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.Rectangle;
 import java.awt.geom.Ellipse2D;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Pelota {
 	private int posx;
@@ -12,16 +14,17 @@ public class Pelota {
 	private double vely;
 	private int radio;
 	private int id;
-	private double angulo;
+	private int velTotal;
 
 	public Pelota(int pPosx, int pPosy, int pId) {
 		id = pId;
 		posx = pPosx;
 		posy = pPosy;
+		velTotal = 7;
 		radio = 12;
-		angulo = Math.toRadians(0);
-		velx = 1 * Math.cos(angulo);
-		vely = 1 * Math.sin(angulo);
+		Double angulo = Math.toRadians(5.0);
+		velx = velTotal * Math.cos(angulo);
+		vely = velTotal * Math.sin(angulo);
 		/*
 		 * if(Math.random() < 0.5) { velx *= -1; }
 		 */
@@ -33,7 +36,7 @@ public class Pelota {
 	}
 
 	public void comprobarParedes(int pAltura, int pAnchura) {
-		if (posy <= 0 + radio || posy >= pAltura - radio * 2) {
+		if ((posy <= 0 && vely < 0) || (posy >= pAltura - radio * 2 && vely > 0)) {
 			vely *= -1;
 		}
 		if (posx <= 0) {
@@ -50,45 +53,32 @@ public class Pelota {
 
 	public void comprobarRaqueta(Raqueta pRaqueta) {
 		if(colision(pRaqueta)) {
-			velx *= -1;
-			if (pRaqueta.getPosx() == 0) {
-				
-				if (pRaqueta.getVelocidad() > 0) {
-					if (angulo < Math.toRadians(70)) {
-						angulo = Math.toRadians(angulo+Math.toRadians(20));
-						velx = 1* Math.cos(angulo);
-						vely = 1* Math.sin(angulo);
-					}
-				}else if(pRaqueta.getVelocidad() < 0) {
-					if (angulo > Math.toRadians(290)) {
-						angulo = Math.toRadians(angulo-Math.toRadians(20));
-						velx = 1* Math.cos(angulo);
-						vely = 1* Math.sin(angulo);
-					}
-				}
-			}else {
-				System.out.println("colision mi pala");
-				if (pRaqueta.getVelocidad() > 0) {
-					if (angulo > Math.toRadians(110)) {
-						System.out.println("angulos");
-						System.out.println(angulo);
-						angulo = Math.toRadians(angulo-Math.toRadians(20));
-						velx = 1* Math.cos(angulo);
-						vely = 1* Math.sin(angulo);
-					}
-				}else if(pRaqueta.getVelocidad() < 0) {
-					if (angulo < Math.toRadians(250)) {
-						System.out.println("angulos");
-						System.out.println(angulo);
-						angulo = Math.toRadians(angulo+Math.toRadians(20));
-						System.out.println(angulo);
-						velx = 1* Math.cos(angulo);
-						vely = 1* Math.sin(angulo);
-					}
-				}
+			
+			if (pRaqueta.getPosx() == 0) {// colisión con raqueta de jugador2
+				if (pRaqueta.getVelocidad() > 0) {//raqueta en movimiento hacía abajo
+					Double angulo = Math.toRadians(ThreadLocalRandom.current().nextInt(10, 80));
+					velx = velTotal* Math.cos(angulo);
+					vely = velTotal* Math.sin(angulo);
+				}else if(pRaqueta.getVelocidad() < 0) {//raqueta en movimiento hacía arriba
+					Double angulo = Math.toRadians(ThreadLocalRandom.current().nextInt(280, 350));
+					velx = velTotal* Math.cos(angulo);
+					vely = velTotal* Math.sin(angulo);
+				}else velx *= -1;
+			
+			}else {// colisión con raqueta de jugador1
+				if (pRaqueta.getVelocidad() > 0) {//raqueta en movimiento hacía abajo
+					Double angulo = Math.toRadians(ThreadLocalRandom.current().nextInt(100, 170));
+					velx = velTotal* Math.cos(angulo);
+					vely = velTotal* Math.sin(angulo);
+				}else if(pRaqueta.getVelocidad() < 0) {//raqueta en movimiento hacía arriba
+					Double angulo = Math.toRadians(ThreadLocalRandom.current().nextInt(190, 260));
+					velx = velTotal* Math.cos(angulo);
+					vely = velTotal* Math.sin(angulo);
+				} else velx *= -1;
 			}
 		}
 	}
+	
 
 	private boolean colision(Raqueta pRaqueta) {
 		if(posy+2*radio >= pRaqueta.getPosy() && posy <= pRaqueta.getPosy()+100) {
@@ -99,7 +89,6 @@ public class Pelota {
 
 			}else {
 				if(posx+2*radio >= pRaqueta.getPosx()) {
-					System.out.println(Math.toDegrees(angulo));
 					if(velx > 0) return true;
 				}
 			}
